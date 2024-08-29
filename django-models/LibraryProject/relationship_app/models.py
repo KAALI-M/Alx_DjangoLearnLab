8cbafe93bb97011e9997866b8cf1a20600e6152e
KAALI Mohammed <kaali.mohammed@outlook.com>
2024-08-29 15:52:18 +0100

    teest

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.db import models
 
 # Create your models here.
@@ -7,10 +8,16 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=150)
     author = models.ForeignKey(Author,on_delete=models.DO_NOTHING)
-
+    def __init__(self):
+        return self.name
+    
 class Library(models.Model):
     name = models.CharField(max_length=150)
     books = models.ManyToManyField(Book)
+    def __init__(self):
+        return self.name
 class Librarian(models.Model):
     name = models.CharField(max_length=150)
     library = models.OneToOneField(Library, on_delete=models.DO_NOTHING)
+    def __init__(self):
+        return self.name
